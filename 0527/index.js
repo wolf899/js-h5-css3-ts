b2c7f3cc4aa8e95e51bdc6eb34fe78a3efe065a0
Fuzhisong <83995105+wolf899@users.noreply.github.com>
2022-04-06 17:33:10 +0800

    js+css

---
+++ 0527/index.js
@@ -0,0 +1,9 @@
+function getId(id){
+    return document.getElementById(id);
+};
+function getClassName(className){
+    return document.getElementsByClassName(className);
+};
+function getTagName(tagName){
+    return document.getElementsByTagName(tagName);
+};
